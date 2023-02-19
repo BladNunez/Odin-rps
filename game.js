1ccd163e1ddd63384b8ac5477af3f2d360f5642e
BladNunez <Nunez.Bladimir@yahoo.com>
2023-02-19 15:38:12 -0500

    create function to return computer choice for RPS game

--- game.js
+++ game.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+//Function to return the computers choice of Rock,Paper,Scissors 
+
+function getComputerChoice(){
+        let computerChoice = ['Rock', 'Paper','Scissors']
+        let randomItem = computerChoice[Math.floor(Math.random() * computerChoice.length)]
+
+        return randomItem;
+}
+
+console.log(getComputerChoice());
