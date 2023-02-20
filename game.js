46e4fcd1c5c0123d5070a38db446cce138115a24
BladNunez <Nunez.Bladimir@yahoo.com>
2023-02-20 15:32:09 -0500

    create loop in game() to play 5 times and display outcome

--- game.js
+++ game.js
@@ -9,8 +9,6 @@ function getComputerChoice(){
 
 //Function to play a round of Rock Paper Scissors
 function playRound(playerSelection,computerSelection){
-        playerSelection = playerSelection.toLowerCase();
-        
         if(playerSelection === "rock" && computerSelection === "Paper"){
                 return 'You Lose! Paper beats Rock'
         } else if(playerSelection === "rock" && computerSelection === "Scissors"){
@@ -28,5 +26,16 @@ function playRound(playerSelection,computerSelection){
         }
 }
 
-let askUser = prompt("Enter your name: ")
-console.log(askUser)
+
+function game(){
+        for(let i = 1; i < 6; i++){
+                let playerGuess = prompt("Enter your Guess: ").toLowerCase();
+                let computerGuess = getComputerChoice();
+
+                console.log(`Round ${i}`)
+                console.log(playRound(playerGuess,computerGuess));
+        }
+        
+}
+
+game();
