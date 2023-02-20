a0984302ec1494df9404094a15df4e7ae0d7df18
BladNunez <Nunez.Bladimir@yahoo.com>
2023-02-20 14:52:24 -0500

    Complete playRound function for all use cases

--- game.js
+++ game.js
@@ -10,11 +10,25 @@ function getComputerChoice(){
 //Function to play a round of Rock Paper Scissors
 function playRound(playerSelection,computerSelection){
         //rock loses paper , rock beats scissors, scissors beats paper
-        if(playerSelection === "rock".toLowerCase() && computerSelection === "Paper" ){
+        playerSelection = playerSelection.toLowerCase();
+        
+        if(playerSelection === "rock" && computerSelection === "Paper"){
                 return 'You Lose! Paper beats Rock'
+        } else if(playerSelection === "rock" && computerSelection === "Scissors"){
+                return 'You Win! Rock beats Scissors'
+        } else if(playerSelection === "paper" && computerSelection === "Rock"){
+                return 'You Win! Paper beats Rock'
+        } else if(playerSelection === "scissors" && computerSelection === "Rock"){
+                return 'You Lose! Rock beats Scissors'
+        } else if(playerSelection === "paper" && computerSelection === "Scissors"){
+                return 'You Lose! Scissors beats Rock'
+        } else if(playerSelection === "scissors" && computerSelection === "Paper"){
+                return 'You Win! Scissors beats Paper'
+        } else {
+                return 'Tie! Same decision on both ends'
         }
 }
 
-const playerSelection = "rock";
+const playerSelection = "scissors";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
